91c7ec05816105d2c4ba60d79e21a934c2215611
Vincent Rainaud <vrainaud.pro@gmail.com>
2024-03-04 22:36:23 +0100

    Fixing deploy so tests can run.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,32 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Get the admin account from the Hardhat node
   const [admin] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", admin.address);
 
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const contractFactory = await ethers.getContractFactory("SmartBet");
+  const smartBet = await contractFactory.deploy();
+  await smartBet.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("SmartBet", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log("SmartBet deployed to:", lock.address);
-  console.log("Admin address:", admin.address);
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Contract deployed to:", smartBet.target);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
